d4c8608164813ea793df4bd865d0e2911e334574
cjty <1042048096@qq.com>
2021-10-21 10:35:06 +0800

    commit

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,7 +35,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'), // 配置访问src 可用 @访问
+      '@': resolve('src'), // 配置访问src 可用 @/访问
     }
   },
   module: {
